69351bcd28537d3f86e5ff9b6e0b16dcc290b51c
Mad At <bhrantiknagar@gmail.com>
2025-11-21 22:43:23 +0530

    mk

---
+++ function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// void printHello();
+// void hello();
+
+// int main() {
+//     printHello();
+//     hello();
+//     return 0;
+// }
+
+// void printHello() {
+//     printf("this is my first function\n");
+// }
+
+// void hello() {
+//     printf("this is my second function\n");
+// }
