28aa369de9264974758750781be99a5c5461e1e5
Mad At <bhrantiknagar@gmail.com>
2025-11-21 23:00:30 +0530

    nj

--- function.c
+++ function.c
@@ -15,3 +15,35 @@
 // void hello() {
 //     printf("this is my second function\n");
 // }
+
+
+
+// void namaste();
+// void hello();
+ 
+// int main() {
+//     printf("Enter your nastionality:\n");
+//     char ch;
+//     scanf("%c",&ch);
+    
+//     if(ch == 'i') {
+//         namaste();
+//     }
+//     else {
+//         hello();
+//     }
+//     return 0;
+
+// }
+ 
+// void namaste() {
+//     printf("Namaste\n");
+// }
+
+// void hello() {
+//     printf("hello\n");
+// }
+
+
+
+
