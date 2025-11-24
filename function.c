30003a43b48dbe94c289051064d49e569d0d76dd
Mad At <bhrantiknagar@gmail.com>
2025-11-24 20:23:21 +0530

    5

--- function.c
+++ function.c
@@ -47,15 +47,40 @@
 
 
 
-int sum (int a, int b);
+// int sum (int a, int b);
 
-int main() {
-    int a, b;
-    printf("enter first nuber : ");
-    scanf("%d",&a);
-    printf("enter second number : ");
-    scanf("%d", &b);
+// int main() {
+//     int a, b;
+//     printf("enter first nuber : ");
+//     scanf("%d",&a);
+//     printf("enter second number : ");
+//     scanf("%d", &b);
 
-    sum 
-}
+//     int s = sum(a, b);
+//     printf("sum is %d :", s);
+//     return 0;
+// }
  
+// int sum(int a, int b) {
+//     return a + b;
+// }
+
+
+
+// #include<math.h>
+
+// void calculateprice(float value);
+
+// int main() {
+//     printf("%f", pow( n, 2));
+
+//     return 0;
+// }
+
+// void calculateprice(float value) {
+//     value = value + (0.18 * value);
+//     printf("final prise is : %f\n", value);
+// }
+
+
+
---
+++ sumarray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void input();
+
+int findsum (int arr [], int n){
+    int i , sum=0; 
+    for (i = 0; i <n; i++) {
+        sum += arr [i];
+    }
+    return sum;
+}
+
+int main(){
+    int sumarr();
+}
+
+
+
