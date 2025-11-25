7948559223332805b472077e6328a81947b4b257
Mad At <bhrantiknagar@gmail.com>
2025-11-26 00:42:41 +0530

    '

--- Loops.c
+++ Loops.c
@@ -151,7 +151,46 @@
 
 
 
+// factorial in loops
+// int main() {
+//     int n;
+//     printf("enter number : ");
+//     scanf("%d", &n);
+
+//     int fact = 1;
+//     for(int i=1; i<=n; i++) {
+//         fact = fact * i;
+//     }
+
+//     printf("final factorial is %d", fact);
+
+//     return 0;
+// }
+
+
+
+// retverse table
+// int main() {
+//     int n;
+//     printf("enter number : ");
+//     scanf("%d", &n);
+
+//     for(int i=10; i>=1; i--) {
+//         printf("%d \n", n * i);
+//     }
+
+//     return 0;
+// }
+
+
+
 int main() {
-    
+    int sum = 0;
+    for(int i=5; i<=50; i++) {
+        sum += i;
+    }
+
+    printf("sum is %d", sum);
+
     return 0;
 }
