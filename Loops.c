b5e84d9a436d998e9f8a857d5795f41f45e3f489
Mad At <bhrantiknagar@gmail.com>
2025-11-26 00:46:56 +0530

    q

--- Loops.c
+++ Loops.c
@@ -184,13 +184,13 @@
 
 
 
-int main() {
-    int sum = 0;
-    for(int i=5; i<=50; i++) {
-        sum += i;
-    }
+// int main() {
+//     int sum = 0;
+//     for(int i=5; i<=50; i++) {
+//         sum += i;
+//     }
 
-    printf("sum is %d", sum);
+//     printf("sum is %d", sum);
 
-    return 0;
-}
+//     return 0;
+// }
