8a61e69e208e5e5d567842e3e2b0168e50169221
Mad At <bhrantiknagar@gmail.com>
2025-11-25 21:14:35 +0530

    .

--- Loops.c
+++ Loops.c
@@ -34,11 +34,43 @@
 
 
 // while loop
-int main() {
-    int i=1;
-    while(i<=5) {
-        printf("hello! while loops\n");
-        i++;
-    }
-    return 0;
-}
+// int main() {
+//     int i=1;
+//     while(i<=5) {
+//         printf("hello! while loops\n");
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+
+// int main() {
+//     int n;
+//     printf("enter number : ");
+//     scanf("%d", &n);
+
+//     // int i=0;
+//     // while(i <= n) {
+//     //     printf("%d \n", i);
+//     //     i++;
+//     // }
+
+
+//     for(int i=0; i<=n; i++) {
+//         printf("%d \n", i);
+//     }
+//     return 0;
+// }
+    
+
+
+// do while loops
+// int main() {
+//     int i=1;
+//     do {
+//         printf("%d\n", i);
+       
+//     } while(i<=5);
+
+// }
