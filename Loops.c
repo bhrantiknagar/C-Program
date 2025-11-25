e496eb3a0e1953f7c67bae8bf3ceeb9380ce1ff8
Mad At <bhrantiknagar@gmail.com>
2025-11-25 19:48:19 +0530

    ,

--- Loops.c
+++ Loops.c
@@ -2,8 +2,43 @@
 
 
 // int main() {
-//     for(int i=100; i>=1; i = i-1) {
+//     for(int i=100; i>=1; i = i++) {
 //         printf("%d \n", i);
 //     }
 // return 0;
 // }
+
+
+
+// int main() {
+//     for( float i =1.0; i<=6.0; i++) {
+//         printf("%f \n", i);
+//     }
+    
+//     for(char ch='a'; ch<='z'; ch++) {
+//         printf("%c \n", ch);
+//     }
+//     return 0;
+// }
+
+
+
+// infinite loops
+// int main(){
+//     for(int i=1; ; i--) {
+//         printf("%d \n", i );
+//     }
+//     return 0;
+// }
+
+
+
+// while loop
+int main() {
+    int i=1;
+    while(i<=5) {
+        printf("hello! while loops\n");
+        i++;
+    }
+    return 0;
+}
