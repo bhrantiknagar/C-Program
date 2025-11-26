c69b7976558ca457dd2d5b14caa457f7191382c5
Mad At <bhrantiknagar@gmail.com>
2025-11-27 00:53:04 +0530

    ]

---
+++ pointers.c
@@ -0,0 +1,58 @@
+#include <stdio.h> 
+
+// int main() {
+//     int age = 22;
+//     int *ptr = &age;
+//     int _age = *ptr;
+
+//     printf("%p\n", &age);
+
+//     return 0;
+// }
+
+
+
+// int main() {
+//     int x;
+//     int *ptr;
+
+//     ptr = &x;
+//     *ptr = 0;     // this line is important
+
+//     printf("x = %d\n", x);
+//     printf("*ptr = %d \n", *ptr);
+
+//     *ptr += 5;
+//     printf("x = %d\n", x);
+//     printf("*ptr = %d \n", *ptr);
+
+//     (*ptr)++;
+//     printf("x = %d\n", x);
+//     printf("*ptr = %d \n", *ptr);
+
+//     return 0;
+// }
+
+
+
+    // pointer to pointer 
+// int main() {
+//     // float price = 100.00;
+//     // float *ptr = &price;
+//     // float **pptr = &ptr;
+
+//     int i =5;
+//     int *ptr =&i;
+//     int **pptr = &ptr;
+
+//     printf("%d\n", **pptr);
+
+//     return 0;
+// }
+
+
+
+int main() {
+    
+    return 0;
+}
