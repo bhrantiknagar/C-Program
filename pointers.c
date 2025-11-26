c86ad71d246b3f87ed738b53a10db6787c9b426e
Mad At <bhrantiknagar@gmail.com>
2025-11-27 02:24:31 +0530

    =

--- pointers.c
+++ pointers.c
@@ -52,23 +52,88 @@
 
 
 
-void square(int n);
-void _square(int* n);
+// void square(int n);
+// void _square(int* n);
 
-int main() {
-    int number = 4;
-    square(number);
-    printf("number = %d\n ", number);
-    return 0;
-}
+// int main() {
+//     int number = 4;
+//     square(number);
+//     printf("number = %d\n ", number);
+//     _square(&number);
+//     printf("number = %d\n", number);
+//     return 0;
+// }
 
-void square(int n) {
-    n = n * n;
-    printf("square = %d\n", n);
-}
+// void square(int n) {
+//     n = n * n;
+//     printf("square = %d\n", n);
+// }
 
-void _square(int* n) {
-    *n = (*n) * (*n);
-    printf("sqaure = %d\n", *n);
-}
+// void _square(int* n) {
+//     *n = (*n) * (*n);
+//     printf("sqaure = %d\n", *n);
+// }
 
+
+
+// swap in pointers
+// void swap(int a, int b);
+// void _swap(int *a, int *b);
+
+// int main() {
+//     int x = 3, y = 5;
+//     _swap(&x, &y);
+//     printf("x = %d & y = %d\n", x, y);
+
+//     return 0;
+// }
+
+// // call by referance
+// void _swap(int *a, int *b) {
+//     int t = *a;
+//     *a = *b;
+//     *b = t;
+// }
+
+// // call by value
+// void swap(int a, int b) {
+//     int t = a;
+//     a = b;
+//     b = t;
+//     printf("a = %d & b = %d\n", a, b);
+// }
+
+
+
+// void printAddress(int *n);
+
+// int main() {
+//     int n = 4;
+//     printAddress(&n);
+//     printf("address of n is : %u\n", &n);
+//     return 0;
+// }
+
+// void printAddress(int *n) {
+//     printf("address of n is : %u\n", &n);
+// }
+
+
+
+// void dowork(int a, int b, int *sum, int *prod, int *avg);
+
+// int main() {
+//     int a = 3, b = 5;
+//     int sum, prod, avg;
+//     dowork(a, b, &sum, &prod, &avg);
+
+//     printf("sum = %d, prod =%d, avg = %d\n", sum, prod, avg);
+
+//     return 0;
+// }
+
+// void dowork(int a, int b, int *sum, int *prod, int *avg) {
+//     *sum = a+b;
+//     *prod = a*b;
+//     *avg = (a+b) /2;
+// }
