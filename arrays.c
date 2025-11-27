cff635ba1d468b2da5c49c9743be09e872c8ddc0
Mad At <bhrantiknagar@gmail.com>
2025-11-27 13:28:29 +0530

    ]

--- arrays.c
+++ arrays.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+
+// int main() {
+     
+//     int marks[3];
+
+//     printf("enter C : ");
+//     scanf("%d", &marks[0]);
+
+//     printf("enter PC : ");
+//     scanf("%d", &marks[1]);
+
+//     printf("enter HTML : ");
+//     scanf("%d", &marks[2]);
+
+//     printf("C = %d, PC = %d, HTML = %d", marks[0], marks[1], marks[2]);
+
+//     return 0;
+// }
+
+
+
+// int main() {
+//     float price[3];
+//     printf("enter 3 prices : ");
+//     scanf("%f", &price[0]);
+//     scanf("%f", &price[1]);
+//     scanf("%f", &price[2]);
+
+//     printf(" total price 1 : %f\n", price[0]+(0.18*price[0]));
+//     printf(" total price 2 : %f\n", price[1]+(0.18*price[1]));
+//     printf(" total price 3 : %f\n", price[2]+(0.18*price[2]));
+
+//     return 0;
+// }
+
+
+
