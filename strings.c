9d01ba6cb3d31df68a5644d0889a1f8bf39eafe5
Mad At <bhrantiknagar@gmail.com>
2025-11-27 18:23:37 +0530

    7

---
+++ strings.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+// int main() {
+//     // char name[] = {'L', 'U', 'C', 'K', 'Y', '\0'};
+//     char name[] = "lucky";
+//     char class[] = "i am learning C ";
+//     return 0;
+// }
